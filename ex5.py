bcafdf43ca92fa0d9878f02d95d58441e6a3a354
hangle2719 <87891485+hangle2719@users.noreply.github.com>
2022-05-08 21:59:17 +0700

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,39 @@
+# my_name = 'Zed A. Shaw'
+# my_age = 35 # not a lie
+# my_height = 74 #inches
+# my_weight = 180 #lbs
+# my_eyes = 'Blue'
+# my_teeth = 'White'
+# my_hair = 'Brown'
+
+# print(f"Let's talk about {my_name}.")
+# print(f"He's {my_height} inches tall.")
+# print(f"He's {my_weight} pounds heavy.")
+# print("Acctually that's not too heavy.")
+# print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+# print(f"His teeth are ussually {my_teeth} depend on the coffee.")
+
+# total = my_age + my_height + my_weight
+# print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 #inches
+height_cm = 2.54 * height
+weight = 180 #lbs
+weight_kgs = weight * 0.453592
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height_cm} cm tall.")
+print(f"He's {weight_kgs} kgs heavy.")
+print("Acctually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are ussually {teeth} depend on the coffee.")
+
+total = age + height_cm + weight_kgs
+print(f"If I add {age}, {height_cm}, and {weight_kgs} I get {total}.")
+
+
